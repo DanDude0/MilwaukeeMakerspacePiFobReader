600a54c1098ef1a7d9b960783b68eaf12e241048
Dan_Dude <dan@vfdworld.com>
2018-09-25 02:32:02 -0500

    Add support for SSDP to automatically locate and connect to API server on local network. Added loading messages to improve developer mood when debugging.

--- MmsPiFobReader/MilwaukeeMakerspaceApiClient.cs
+++ MmsPiFobReader/MilwaukeeMakerspaceApiClient.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Net.Http;
+using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Rssdp;
 
 namespace MmsPiFobReader
 {
@@ -8,10 +10,12 @@ namespace MmsPiFobReader
     {
         HttpClient client;
 
-        public MilwaukeeMakerspaceApiClient()
+		public MilwaukeeMakerspaceApiClient()
         {
+			var host = SearchForServer().Result;
+
             client = new HttpClient();
-            client.BaseAddress = new Uri("http://10.1.1.15/");
+            client.BaseAddress = new Uri($"http://{host}/");
             client.Timeout = new TimeSpan(0, 0, 1);
 
             var unused = client.GetStringAsync($"/").Result;
@@ -30,5 +34,20 @@ namespace MmsPiFobReader
 
             return JsonConvert.DeserializeObject<AuthenticationResult>(result);
         }
-    }
+
+		public async Task<string> SearchForServer()
+		{
+			using (var deviceLocator = new SsdpDeviceLocator())
+			{
+				var foundDevices = await deviceLocator.SearchAsync("uuid:6111f321-2cee-455e-b203-4abfaf14b516", new TimeSpan(0, 0, 5));
+
+				foreach (var foundDevice in foundDevices)
+				{
+					return foundDevice.DescriptionLocation.Authority;
+				}
+			}
+
+			throw new Exception("Could not locate server");
+		}
+	}
 }
