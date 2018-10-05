98c14252f21d85132de7516aebc0e1810ad16eac
Dan_Dude <dan@vfdworld.com>
2018-10-05 01:22:54 -0500

    Improvements to SSDP

--- MmsPiFobReader/MilwaukeeMakerspaceApiClient.cs
+++ MmsPiFobReader/MilwaukeeMakerspaceApiClient.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Net;
 using System.Net.Http;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Rssdp;
@@ -35,19 +38,68 @@ namespace MmsPiFobReader
             return JsonConvert.DeserializeObject<AuthenticationResult>(result);
         }
 
-		public async Task<string> SearchForServer()
-		{
-			using (var deviceLocator = new SsdpDeviceLocator())
-			{
-				var foundDevices = await deviceLocator.SearchAsync("uuid:6111f321-2cee-455e-b203-4abfaf14b516", new TimeSpan(0, 0, 5));
+		private SsdpDeviceLocator deviceLocator = null;
+		private string hostname = null;
 
-				foreach (var foundDevice in foundDevices)
-				{
-					return foundDevice.DescriptionLocation.Authority;
+		private async Task<string> SearchForServer()
+		{
+			var ip4 = GetLocalIp4Address();
+
+			try{
+				deviceLocator = new SsdpDeviceLocator(ip4);
+				deviceLocator.StartListeningForNotifications();
+				deviceLocator.DeviceAvailable += FoundDevice;
+				var task = deviceLocator.SearchAsync(new TimeSpan(0, 0, 10));
+
+				while (deviceLocator.IsSearching) {
+					if (hostname != null)
+						return hostname;
+
+					await Task.Delay(500);
 				}
+
+			}
+			finally
+			{
+				deviceLocator.StopListeningForNotifications();
 			}
 
 			throw new Exception("Could not locate server");
+		}
+
+		private void FoundDevice(object sender, DeviceAvailableEventArgs e)
+		{
+			if (e.DiscoveredDevice.Usn.Contains("6111f321-2cee-455e-b203-4abfaf14b516"))
+				hostname = e.DiscoveredDevice.DescriptionLocation.Host;
+		}
+
+		private static string GetLocalIp4Address()
+		{
+			var networkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
+
+			foreach (var network in networkInterfaces)
+			{
+				if (network.OperationalStatus != OperationalStatus.Up)
+					continue;
+
+				var properties = network.GetIPProperties();
+
+				if (properties.GatewayAddresses.Count == 0)
+					continue;
+
+				foreach (var address in properties.UnicastAddresses)
+				{
+					if (address.Address.AddressFamily != AddressFamily.InterNetwork)
+						continue;
+
+					if (IPAddress.IsLoopback(address.Address))
+						continue;
+
+					return address.Address.ToString();
+				}
+			}
+
+			throw new Exception("No IP Address Found for SSDP");
 		}
 	}
 }
