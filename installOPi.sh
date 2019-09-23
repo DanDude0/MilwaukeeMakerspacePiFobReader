32b4ac3138954cefb7fbd814e2bf7e2df2e47060
Dan_Dude <dan@vfdworld.com>
2019-09-22 23:57:30 -0500

    Fix bugs

--- installOPi.sh
+++ installOPi.sh
@@ -51,7 +51,7 @@ apt-get -y install git libunwind8 busybox-syslogd ntp libgpiod-dev gpiod
 # Remove unneeded libraries
 heading 'Removing Libraries'
 apt-get -y purge anacron unattended-upgrades logrotate dphys-swapfile rsyslog
-apt-get autoremove
+apt-get -y autoremove
 
 # Setup Screen Hardware
 heading 'Setting Up Screen Hardware'
@@ -65,7 +65,7 @@ echo 'options fbtft_device rotate=270 name=piscreen speed=16000000 gpios=reset:2
 
 # Install SSH Key
 heading 'Installing SSH Key'
-mkdir ~/.ssh && touch ~/.ssh/authorized_keys
+mkdir -p ~/.ssh && touch ~/.ssh/authorized_keys
 chmod 700 ~/.ssh && chmod 600 ~/.ssh/authorized_keys
 echo 'ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAgEAvaHWKFt0zD0HAiv/PrTT/Qx0g4RxRAnbCrxO2C9oqPifXtcuKTW0aWNp0NyQMEQz/KFLZtzyRJi3A7rjEOH/6qotuhV7T5KJT39OgCL4A4vK1e4R+h8ZmitgIPGR5HYzW8X+1ODhGhO04nLh7LELHdYxy9gZWaMFNC3F1hlkCJF3WepkMClFoqt4pv8UHA4yWjS8eaudvcdPnoSdO2bAAuoCao/tB78dJomkxV8laDENb5wgwqNv/1p3yxTK+y1srxRcJyr6dFS6us9puVEfbpWFuMrW9v/NxylcCa1TOp1F3iKCkg8WQQPNbTC7drIh9J8Ntma/1wFPsFHfQcQ0oqgz0QDK/z8F1w1PQmh+exlK5gk0769Zt4EaTUJ8xDFf++fBIFVgGE8FnLjHRmaNfGWe3Uw0Kd9pzbXfOmcqMxvKCapTKG0c/j7fOJUIXE4BIYFB00FezEQs4e5d+rZ5Pc6SW3ue/4fB3+JoI4Kh1C2lx7SreODhgkc+uLYK31W/Z/jtJ3CwYSLz2EIdwJXLGHyPavhTxLi6mhEUpFNI85NUIOBxc2Fhx34kDEbrGPL7tiLiJo4ZfIVEog8ghgcBnxVuDFlMi/poAU8cLO2USAx0XCkF2kkyeuiBHuze/qzjO0YEXxeEapgbJnDIO0kJHONiLmjAV0DysZ1sNV8EBhs= MmsPiFobReader' > ~/.ssh/authorized_keys
 
@@ -90,11 +90,15 @@ chmod +x build.sh
 # Install MmsPiFobReader
 heading 'Installer MmsPiFobReader'
 cd /tmp
+mv /opt/MmsPiFobReader/readerid.txt /tmp
+mv /opt/MmsPiFobReader/server.txt /tmp
 rm -rfv /opt/MmsPiFobReader/*
 curl -s https://api.github.com/repos/DanDude0/MilwaukeeMakerspacePiFobReader/releases/latest | grep -P "(?<=browser_download_url\": \")https://.*zip" -o | wget -i -
 unzip -o MmsPiFobReader.zip -d /opt/MmsPiFobReader
 rm -f MmsPiFobReader.zip
 chmod +x /opt/MmsPiFobReader/MmsPiFobReader
+mv /tmp/readerid.txt /opt/MmsPiFobReader/
+mv /tmp/server.txt /opt/MmsPiFobReader/
 
 # Install Systemd Unit
 heading 'Installing Systemd Units'
@@ -134,4 +138,5 @@ END
 
 systemctl enable MmsPiFobReader MmsPiW26Interface
 
-echo 'Install completed, reboot to start reader';
+echo 'Install completed, rebooting to start reader';
+reboot
